2965d115ff172bba725a02b73cee9344032feda9
GREEN <GREEN@G704-00>
2024-08-12 11:07:41 +0900

    template 상속하기 구현완료

--- mysite_17th/src/test/java/com/study/mysite/Mysite17thApplicationTests.java
+++ mysite_17th/src/test/java/com/study/mysite/Mysite17thApplicationTests.java
@@ -3,6 +3,7 @@ package com.study.mysite;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,7 +27,7 @@ class Mysite17thApplicationTests {
 	@Autowired
 	private AnswerRepository answerRepository;
 	
-	@Transactional
+	
 	@Test
 	void testJpa() {
 		/*
@@ -51,23 +52,24 @@ class Mysite17thApplicationTests {
 		List<Question> all = this.questionRepository.findAll();
 		assertEquals(3, all.size());
 		
+		
 		Question q = all.get(0);
 		assertEquals("JPA가 무엇인가요?", q.getSubject());
 		
 		
 		Question q = this.questionRepository.findBySubject("ORM이 무엇인가요?");
-		assertEquals(3, q.getId());
+		assertEquals(8, q.getId());
 		
 		
 		Question q1 = this.questionRepository.findBySubjectAndContent("ORM이 무엇인가요?", "궁금합니다.");
-		assertEquals(3, q1.getId());
+		assertEquals(8, q1.getId());
 		
 		
 		List<Question> qList = this.questionRepository.findBySubjectLike("%무엇%");
 		this.questionRepository.findAll();
 		
 		
-		Optional<Question> oq = this.questionRepository.findById(2);
+		Optional<Question> oq = this.questionRepository.findById(8);
 		assertTrue(oq.isPresent());
 		Question q2 = oq.get();
 		q2.setSubject("ORM이 무엇인가요?");
@@ -80,8 +82,9 @@ class Mysite17thApplicationTests {
 		Question q = oq.get();
 		this.questionRepository.delete(q);
 		assertEquals(2, this.questionRepository.count());
+		*/
 		
-		Optional<Question> oq = this.questionRepository.findById(3);
+		Optional<Question> oq = this.questionRepository.findById(7);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 		
@@ -90,15 +93,18 @@ class Mysite17thApplicationTests {
 		a.setQuestion(q); //어떤 질문의 답변인지 설정해야 하기 때문에 Question 객체가 필요하다!!
 		a.setCreateDate(LocalDateTime.now());
 		this.answerRepository.save(a);
-		*/
-		Optional<Question> oq = this.questionRepository.findById(3);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
+		
+		/*
+		Optional<Question> oq2 = this.questionRepository.findById(3);
+		assertTrue(oq2.isPresent());
+		Question q2 = oq2.get();
+		
 		
 		List<Answer> answerList = q.getAnswerList();
 		//테스트 코드를 통과하지 못한 이유는 answerList는 q객체를 조회할 때가 아니라 getAnswerList()메소드를 호출하는 시점에 가져오기 때문에 에러가 난다. 이렇게 데이터를 필요한 시점에 가져오는 방식을 지연(lazy)방식이라고 한다. <=>이와 반대로 q객체를 조회할 때 미리 answer 리스트를 모두 가져오는 방식을 즉시(Eager)방식이라고 한다.=>테스트 코드시 이런 오류를 방지할 수 있는 가장 간단한 방법:@Transactional 애너테이션을 사용하여 DB세션이 끊기지 않고 계속 유지가 되어 해결!!
 		assertEquals(1, answerList.size());
 		assertEquals("스프링부트에서 사용하는 JPA 인터페이스의 실제 구현체이다.", answerList.get(0).getContent());
+		*/
 		
 	}
 
