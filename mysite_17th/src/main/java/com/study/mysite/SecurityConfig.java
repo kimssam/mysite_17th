47a134a006e2789444bdda6689e28513027c42ae
GREEN <GREEN@G704-00>
2024-08-14 17:07:22 +0900

    스프링 시큐리티를 이용한 회원가입, 회원 로그인 완료

--- mysite_17th/src/main/java/com/study/mysite/SecurityConfig.java
+++ mysite_17th/src/main/java/com/study/mysite/SecurityConfig.java
@@ -2,13 +2,14 @@ package com.study.mysite;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
-import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -35,6 +36,12 @@ public class SecurityConfig {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+		return authenticationConfiguration.getAuthenticationManager();
+		//사용자 인증과 권한 부여 프로세스를 내부적으로 처리한다.
 	}
 }
 
--- mysite_17th/src/main/java/com/study/mysite/user/UserRepository.java
+++ mysite_17th/src/main/java/com/study/mysite/user/UserRepository.java
@@ -1,7 +1,9 @@
 package com.study.mysite.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long>{
-	
+	Optional<SiteUser> findByusername(String username);
 }
